fb894832645b96a876ab66e93d6adc3387cd631b
ynakahashi <ynakahashi1003@gmail.com>
2019-01-10 19:40:16 +0900

    Finished Test 1-1

--- sample_define.rkt
+++ sample_define.rkt
@@ -1,4 +1,28 @@
 #lang racket
 
+; Variable can be defined by "define"
 (define size 2)
-size
+(* 5 size)
+(define radius 10)
+(* pi (* radius radius))
+(define (sq x) (* x x))
+(sq 10)
+(define (sumsq x y) (+ (* x x) (* y y)))
+(sumsq 5 6)
+
+; Use "cond" for condition
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+(abs -4)
+(define (abs2 x)
+  (cond ((< x 0) (- x))
+        (else x)))
+(abs2 -5)
+; Can use "if" if a condition has only 2 pattern
+(define (abs3 x)
+  (if (< x 0) (- x) x))
+(abs3 -6)
+
+; and, or, not
